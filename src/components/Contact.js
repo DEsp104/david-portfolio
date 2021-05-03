445ca4bdda6728b372a457528c0e0762b489f44e
david espinal <despinal0425@gmail.com>
2021-05-03 12:39:58 -0400

    added the logo for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
-
+import github from "../images/contact/github.svg";
+import linkedin from "../images/contact/linkedin.svg";
+import mail from "../images/contact/mail.svg";
 
 
 
@@ -12,6 +14,13 @@ export default function Home() {
   return (
     <div id="contact">
       <h2>Contact</h2>
+      <div class="underline"></div>
+      <ul>
+        <li><img src={github} alt="" /></li>
+        <li><img src={mail} alt="" /></li>
+        <li><img src={linkedin} alt="" /></li>
+      </ul>
+      
     </div>
   )
 }
