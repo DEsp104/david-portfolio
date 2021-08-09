e497b972cf43fc62c1a47f9da9b6f0b77c521686
David Espinal <despinal0425@gmail.com>
2021-08-08 23:09:18 -0400

    added about font to header and background color

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,13 @@ export default function About() {
 
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <div className="underline"></div>
-      <p>I'm a full-stack developer from New York City with experience in JavaScript and modern frameworks like React and React Native. Aside from putting my JavaScript skills into practice, I utilize my passion for technology to create user-friendly applications with my art and design skills, as well as web development abilities. In my spare time, I enjoy recreational acitivities such as bike riding and playing basketball.</p>
+      <div className="background-img">
+        <div  className="background-color">
+        <h2>About Me</h2>
+        <div className="underline"></div>
+        <p>I'm a full-stack developer from New York City with experience in JavaScript and modern frameworks like React and React Native. Aside from putting my JavaScript skills into practice, I utilize my passion for technology to create user-friendly applications with my art and design skills, as well as web development abilities. In my spare time, I enjoy recreational acitivities such as bike riding and playing basketball.</p>
+        </div>
+      </div>  
     </div>
   )
 }
